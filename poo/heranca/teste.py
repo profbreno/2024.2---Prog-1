86c2e864ec41c88336dfdd2244827f53aaee6ea4
Breno Araújo <brenod123@gmail.com>
2024-11-11 10:10:05 -0300

    Update teste.py

--- poo/heranca/teste.py
+++ poo/heranca/teste.py
@@ -6,6 +6,16 @@ gato = Gato('Tom', 5, 'Siames')
 cachorro = Cachorro('Rex', 3, 'Pastor Alemão')
 ave = Ave('Piu', 1, 'Calopsita')
 
-print(gato.emitir_som())
-print(cachorro.emitir_som())
-print(ave.emitir_som())
+#print(gato.emitir_som())
+#print(cachorro.emitir_som())
+#print(ave.emitir_som())
+
+lista = list()
+lista.append(gato)
+lista.append(cachorro)
+lista.append(ave)
+
+for animal in lista:
+    print('------------------')
+    print(animal.emitir_som())
+    print('------------------')
