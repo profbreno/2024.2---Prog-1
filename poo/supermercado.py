da6b121a0edc092222c9e783248cb251c6de30c8
Breno Araújo <brenod123@gmail.com>
2025-01-28 08:14:42 -0300

    Update supermercado.py

--- poo/supermercado.py
+++ poo/supermercado.py
@@ -25,7 +25,7 @@ class ProdutoEstoque(Produto):
         return self._descricao
     
     @descricao.setter
-    def preco(self, descricao):
+    def descricao(self, descricao):
         self._descricao = descricao
     
     def entrada(self, quantidade):
@@ -105,4 +105,106 @@ class Caixa:
         escolha = input("Informe sua opção: ").upper()
         return escolha
         
-Caixa.menu()
+    
+    def busca_produto(self):
+        if len(self._produtos) == 0:
+            print('Nenhum produto cadastrado!')
+            return None
+        print('\nProdutos:')
+        for cod, produto in self._produtos.items():
+            print(cod, ':', produto)
+        codigo = pergunta('Código do produto: ')
+        if codigo in self._produtos:
+            return self._produtos[codigo]
+        print('Produto não encontrado!')
+        return None
+    
+    @classmethod
+    def dados_produto(cls):
+        print('\nInforme os dados')
+        descricao = input('Descrição: ')
+        preco = pergunta('Preço: ', float)
+        return descricao, preco
+        
+    def cadastro_produto(self):
+        produto = self.busca_produto()
+        if produto is not None:
+            print('Produto cadastrado:', produto)
+            if confirma('Alterar? (S/N) ', 'S'):
+                descricao, preco = self.dados_produto()
+                produto.descricao = descricao
+                produto.preco = preco
+        else:
+            if confirma('Incluir? (S/N) ', 'S'):
+                descricao, preco = self.dados_produto()
+                produto = ProdutoEstoque(descricao, preco)
+                print(descricao, preco)
+                codigo = len(self._produtos)
+                self._produtos[codigo] = produto
+        
+    def entrada_estoque(self):
+        produto = self.busca_produto()
+        if produto is not None:
+            quantidade = pergunta('Quantidade de entrada: ', float)
+            produto.entrada(quantidade)
+            
+    def venda(self):
+        print('Venda')
+        venda = Venda()
+        while True:
+            produto = self.busca_produto()
+            if produto is not None:
+                quantidade = pergunta('Quantidade vendida: ', float)
+                if produto.saida(quantidade):
+                    produto_venda = ProdutoVenda(produto.descricao,
+                                                    produto.preco,
+                                                    quantidade)
+                    venda.adiciona_produto(produto_venda)
+            print(venda)
+            if confirma('Adicionar mais produtos? (S/N) ', 'N'):
+                break
+        if venda.numero_produtos > 0:
+            self._vendas.append(venda)
+            
+    def relatorio_vendas(self):
+        if len(self._vendas) == 0:
+            print('Nenhuma venda encontrada!')
+            return
+        total_geral = 0
+        for cont, venda in enumerate(self._vendas):
+            print('\nVenda', cont)
+            print(venda)
+            total_geral += venda.total
+        print('TOTAL GERAL:', total_geral)
+        input('Pressine ENTER para voltar')
+        
+    def iniciar(self):
+        while True:
+            escolha = self.menu()
+            if escolha == 'C':
+                self.cadastro_produto()
+            elif escolha == 'E':
+                self.entrada_estoque()
+            elif escolha == 'V':
+                self.venda()
+            elif escolha == 'R':
+                self.relatorio_vendas()
+            elif escolha == 'S':
+                break
+
+def pergunta(mensagem, tipo=int):
+    while True:
+        try:
+            resp = input(mensagem)
+            return tipo(resp)
+        except:
+            print('Valor inválido! Informe novamente.')
+
+def confirma(mensagem, resposta):
+    texto = input(mensagem).strip()
+    if texto.lower() == resposta.lower():
+        return True
+    return False
+
+if __name__ == '__main__':
+    Caixa().iniciar()
